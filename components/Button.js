747a20ce64e61685e3401fff4ee0727c718ad1d8
John Monk <johntmonk1@gmail.com>
2023-02-01 18:23:53 -0600

    added markup and styling for button component

--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = () => {
+const Button = ({children}) => {
     return (
-        <div>
-            
-        </div>
+        <StyledButton>
+            {children}
+        </StyledButton>
     );
 }
+
+const StyledButton = styled.button`
+    all: unset;
+    font-size: 1rem;
+    font-weight: 600;
+    color: ${props => props.theme.colors.white};
+    background: ${props => props.theme.colors.orange};
+    width: 90%;
+    margin: 0 auto;
+    height: 3.5em;
+    border-radius: 0.5em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
 
 export default Button;
