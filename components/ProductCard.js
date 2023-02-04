53214b1a928d1687a9ad9178d210486657fa9657
John Monk <johntmonk1@gmail.com>
2023-02-03 18:43:29 -0600

    removed previous styling and re-styled component to be compatible with new markup

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -35,198 +35,82 @@ const Wrapper = styled.section`
   .product {
     display: flex;
     flex-direction: column;
-    padding: 1.5em;
+    padding: 1em 1.5em;
     gap: 1em;
 
+    &__brand {
+      text-transform: uppercase;
+      font-weight: 600;
+      font-size: 0.7rem;
+      letter-spacing: 0.2em;
+      color: ${(props) => props.theme.colors.orange};
+    }
+
+    &__model {
+      font-size: 1.7rem;
+    }
+
     &__info {
+      line-height: 1.7;
+      color: ${(props) => props.theme.colors.darkGrayBlue};
+      font-size: 0.95rem;
+    }
+
+    &__pricing {
       display: flex;
-      flex-direction: column;
-      gap: 1em;
+      justify-content: space-between;
+      align-items: center;
 
-      &--brand {
-        color: ${(props) => props.theme.colors.orange};
-        text-transform: uppercase;
+      .price {
         font-weight: 600;
-        font-size: 0.75rem;
-        letter-spacing: 0.1em;
-      }
-
-      &--title {
         font-size: 1.7rem;
+        display: flex;
+        align-items: center;
+        gap: 0.5em;
       }
 
-      &--text {
-        font-size: 0.9rem;
-        line-height: 1.6;
-        color: ${(props) => props.theme.colors.darkGrayBlue};
+      .discount {
+        font-size: 1rem;
+        color: ${(props) => props.theme.colors.orange};
+        background: ${(props) => props.theme.colors.lightOrange};
+        padding: 0.2em 0.5em;
+        border-radius: 0.4em;
+      }
+
+      .original {
+        font-weight: 600;
+        color: ${(props) => props.theme.colors.grayBlue};
+        text-decoration: line-through;
       }
     }
 
     &__purchase {
       display: flex;
-      align-items: center;
-      justify-content: space-between;
       flex-direction: column;
       gap: 1em;
-      flex-wrap: wrap;
 
-      &--pricing {
+      .quantity {
         display: flex;
         align-items: center;
+        height: 4em;
+        background: ${props => props.theme.colors.lightGrayBlue};
+        border-radius: 0.5em;
         justify-content: space-between;
-        width: 100%;
+        padding: 0 1em;
 
-        .original__price {
-          font-weight: 600;
-          color: ${(props) => props.theme.colors.grayBlue};
-          text-decoration: line-through;
+        .btn {
+          display: flex;
         }
 
-        .price {
+        .display {
           font-weight: 600;
-          font-size: 1.8rem;
-          display: flex;
-          align-items: center;
-          gap: 0.5em;
-
-          .discount {
-            font-size: 1rem;
-            color: ${(props) => props.theme.colors.orange};
-            background: ${(props) => props.theme.colors.lightOrange};
-            padding: 0.2em 0.5em;
-          }
-        }
-      }
-
-      .purchase {
-        width: 100%;
-
-        .button__container {
-            width: 100%;
-          }
-
-
-        .quantity {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding: 1em;
-          width: 100%;
-          background: ${(props) => props.theme.colors.lightGrayBlue};
-
-          .display {
-            font-weight: 600;
-          }
-
-          .minus,
-          .plus {
-            all: unset;
-            cursor: pointer;
-            width: 1.5em;
-            height: 1.5em;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
         }
       }
     }
-  }
-
-  @media screen and (min-width: 640px) {
-    .product {
-      flex-direction: row;
-      padding: 2em;
-      gap: 3em;
-
-      &__info {
-        &--brand {
-          font-size: 1rem;
-        }
-
-        &--title {
-          font-size: 2rem;
-        }
-
-        &--text {
-          font-size: 1.1rem;
-        }
-      }
-
-      &__purchase {
-        justify-content: flex-end;
-        gap: 2em;
-      }
-
-      Button {
-        align-self: flex-start;
-        width: 50%;
-      }
-    }
-  }
-
-  @media screen and (min-width: 768px) {
-    .product {
-      flex-direction: column;
-      width: 35em;
-      gap: 2em;
-
-      &__info {
-        &--title {
-          font-size: 3rem;
-        }
-      }
-
-      &__purchase {
-
-        &--pricing {
-          flex-direction: column;
-          align-items: flex-start;
-        }
-      }
-
-      .purchase {
-        display: flex;
-        justify-content: space-between;
-        gap: 2em;
-
-
-        .button__container {
-          display: flex;
-          flex-direction: row;
-          justify-content: flex-end;
-
-          Button {
-            width: 17em;
-            margin: 0;
-          }
-        }
-
-
-        .quantity {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding: 1em;
-          width: 100%;
-          background: ${(props) => props.theme.colors.lightGrayBlue};
-
-          .display {
-            font-weight: 600;
-          }
-
-          .minus,
-          .plus {
-            all: unset;
-            cursor: pointer;
-            width: 1.5em;
-            height: 1.5em;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-        }
-      }
+    .add {
+      width: 100%;
+      gap: 0.8em;
+      align-items: center;
     }
   }
 `;
