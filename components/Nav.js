8e567cd8fcb1cd1fe8133409f60b15061e762216
John Monk <johntmonk1@gmail.com>
2023-02-03 20:58:18 -0600

    added active and inactive classes to nav items that toggle on click, these trigger a pseudo element indicating current menu status

--- components/Nav.js
+++ components/Nav.js
@@ -14,16 +14,16 @@ const Nav = ({toggleOverlay}) => {
     }
 
     return (
-        <Wrapper>
+        <Wrapper className={className}>
             <Image onClick={handleNav} className='menu__open btn' src={menu} />
             <nav className={`nav ${nav ? 'nav__open' : ''}`}>
                 <Image onClick={handleNav} className='menu__close btn' src={close} />
                 <ul className='nav__list'>
-                    <li className='nav__list--item'><a href="#">Collections</a></li>
-                    <li className='nav__list--item'><a href="#">Men</a></li>
-                    <li className='nav__list--item'><a href="#">Women</a></li>
-                    <li className='nav__list--item'><a href="#">About</a></li>
-                    <li className='nav__list--item'><a href="#">Contact</a></li>
+                    <li className='nav__list--item'><a onClick={() => setActive('collections')} className={`${ active === 'collections' ? 'active' : 'inactive' }`} href="#">Collections</a></li>
+                    <li className='nav__list--item'><a onClick={() => setActive('men')} className={`${ active === 'men' ? 'active' : 'inactive' }`} href="#">Men</a></li>
+                    <li className='nav__list--item'><a onClick={() => setActive('women')} className={`${ active === 'women' ? 'active' : 'inactive' }`} href="#">Women</a></li>
+                    <li className='nav__list--item'><a onClick={() => setActive('about')} className={`${ active === 'about' ? 'active' : 'inactive' }`} href="#">About</a></li>
+                    <li className='nav__list--item'><a onClick={() => setActive('contact')} className={`${ active === 'contact' ? 'active' : 'inactive' }`} href="#">Contact</a></li>
                 </ul>
             </nav>
         </Wrapper>
@@ -31,65 +31,105 @@ const Nav = ({toggleOverlay}) => {
 }
 
 const Wrapper = styled.div`
-    .nav {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100vh;
-        background: ${props => props.theme.colors.white};
-        width: 70%;
-        padding: 1.5em;
-        transform: translateX(-100%);
-        transition: 200ms;
-        z-index: 2;
+  .nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    background: ${(props) => props.theme.colors.white};
+    width: 70%;
+    padding: 1.5em;
+    transform: translateX(-100%);
+    transition: 200ms;
+    z-index: 2;
 
-        &__open {
-            transform: translateX(0);
-            transition: 200ms;
-        }
-
-        &__list {
-            padding-top: 2em;
-
-            &--item {
-                padding: 0.5em 0;
-                font-size: 1rem;
-                font-weight: 600;
-                color: ${props => props.theme.colors.veryDarkBlue};
-            }
-        }
+    &__open {
+      transform: translateX(0);
+      transition: 200ms;
     }
 
+    &__list {
+      padding-top: 2em;
+
+      &--item {
+        padding: 0.5em 0;
+        font-size: 1rem;
+        font-weight: 600;
+        color: ${(props) => props.theme.colors.veryDarkBlue};
+      }
+    }
+  }
+
+  .btn {
+    cursor: pointer;
+  }
+
+  @media screen and (min-width: 768px) {
     .btn {
-        cursor: pointer;
+      display: none;
     }
 
-    @media screen and (min-width: 768px) {
-        .btn {
-            display: none;
+    .nav {
+      position: unset;
+      height: unset;
+      background: unset;
+      width: unset;
+      padding: unset;
+      transform: unset;
+
+      &__list {
+        display: flex;
+        padding: unset;
+        gap: 2em;
+
+        &--item {
+          padding: unset;
+          font-weight: 400;
+          font-size: 0.9rem;
         }
+      }
+    }
+  }
 
-        .nav {
-            position: unset;
-            height: unset;
-            background: unset;
-            width: unset;
-            padding: unset;
-            transform: unset;
+  @media screen and (min-width: 900px) {
+    .nav {
+      &__list {
+        &--item {
+          color: ${(props) => props.theme.colors.grayBlue};
 
-            &__list {
-                display: flex;
-                padding: unset;
-                gap: 2em;
+          a {
+            position: relative;
 
-                &--item {
-                    padding: unset;
-                    font-weight: 400;
-                    font-size: 0.9rem;
-                }
+            &::after {
+              border-bottom: 4px solid ${props => props.theme.colors.orange};
+              width: 100%;
+              bottom: 0;
+              left: 0;
+              transform: translateY(3.1em);
+              content: '';
+              position: absolute;
+              opacity: 0;
             }
+
+            &:hover {
+              color: ${props => props.theme.colors.darkGrayBlue};
+
+              &::after {
+                opacity: 1;
+              }
+            }
+          }
+
+          .active {
+            color: ${props => props.theme.colors.darkGrayBlue};
+            &::after {
+                opacity: 1;
+              }
+          }
         }
+      }
     }
-`
+  }
+`;
 
 export default Nav;
